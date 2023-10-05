e70b3bf5659619a6f17ba1841114feb20aa8e18b
Yerin Kim <yrkim.me@gmail.com>
2023-10-05 21:55:05 +0900

    초당 한 번 알려주는 시계

--- TCP/time_client.py
+++ TCP/time_client.py
@@ -1,6 +1,10 @@
 import socket
+import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 address = ("localhost", 5000)
 sock.connect(address) # 서버 연결
-print("현재 시각: ", sock.recv(1024).decode()) # 수신 내용을 문자열로 변환하여 출력
+
+while True:
+    time.sleep(1)
+    print("현재 시각: ", sock.recv(1024).decode()) # 수신 내용을 문자열로 변환하여 출력
--- TCP/time_server.py
+++ TCP/time_server.py
@@ -7,8 +7,8 @@ address = ('', 5000)
 s.bind(address) # 소켓과 주소 결합
 s.listen(5) # 연결 대기. 5개까지 동시 수용
 
+client, addr = s.accept() # 연결 허용
+
 while True:
-    client, addr = s.accept() # 연결 허용
     print("Connection requested from ", addr)
     client.send(time.ctime(time.time()).encode()) # 현재 시간 전송
-    client.close() # 소켓 종료
