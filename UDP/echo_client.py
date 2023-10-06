0de1bbeeb7d53959135169764d35bda823253818
Yerin Kim <yrkim.me@gmail.com>
2023-10-06 13:17:35 +0900

    1:1 chat

--- UDP/echo_client.py
+++ UDP/echo_client.py
@@ -5,7 +5,8 @@ port = 2500
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-msg = input("message: ")
-sock.sendto(msg.encode(), ('localhost', port)) # 메시지 송신
-data, addr = sock.recvfrom(BUFSIZE) # 메시지 수신
-print("Server says: ", data.decode())
+while True:
+    msg = input("message: ")
+    sock.sendto(msg.encode(), ('localhost', port)) # 메시지 송신
+    data, addr = sock.recvfrom(BUFSIZE) # 메시지 수신
+    print("Server says: ", data.decode())
--- UDP/echo_server.py
+++ UDP/echo_server.py
@@ -9,4 +9,6 @@ sock.bind(('', port))
 while True:
     data, addr = sock.recvfrom(BUFSIZE)
     print("Received message: ", data.decode())
-    sock.sendto(data, addr)
+
+    s_msg = input("message: ")
+    sock.sendto(s_msg.encode(), addr)
